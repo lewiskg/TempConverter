4959e978d980142f638c63657497eb91919638aa
ken lewis <kenneth.g.lewis2@gmail.com>
2017-08-02 20:53:17 -0500

    work in progress

---
+++ converter.js
@@ -0,0 +1,43 @@
+console.log(parseInt(document.getElementById("inputTemp").value));
+
+var inputTemp = parseInt(document.getElementById("inputTemp").value);
+
+while (isNaN(inputTemp)) {
+	
+}
+
+/*
+function determineConverter (inputTemp) {
+ 	var outputTemp = 0;
+ 	if (document.getElementById("F") !== 'undefined') {
+ 		outputTemp = toFahrenheit(inputTemp);
+ 	} else {
+ 		outputTemp = toCelsius(inputTemp);
+ 	}
+}
+
+function toCelsius (inputTemp) {
+	var Tf = inputTemp * (9/5) + 32;
+	return Tf;
+}
+
+function toFahrenheit (inputTemp) {
+	var Tc = (inputTemp - 32) * (5/9);
+	return Tc;
+}
+
+function outPutToScreen ()
+
+// Get a reference to the button element in the DOM
+var button = document.getElementById("converter");
+
+// This function should determine which conversion should
+// happen based on which radio button is selected.
+function determineConverter (clickEvent) {
+  console.log("event", clickEvent);
+}
+
+// Assign a function to be executed when the button is clicked
+button.addEventListener("click", determineConverter());
+
+*/
