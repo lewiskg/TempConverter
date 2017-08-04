fb0b35261f716b451a0feb55d0a2657487cff792
Kenneth Lewis <lewiskg@Kenneths-iMac.local>
2017-08-04 07:26:00 -0500

    still working on it

--- converter.js
+++ converter.js
@@ -1,3 +1,16 @@
+// Get a reference to the button element in the DOM
+var button = document.getElementById("converter");
+
+// This function should determine which conversion should
+// happen based on which radio button is selected.
+//function determineConverter (clickEvent) {
+//  console.log("event", clickEvent);
+//}
+
+// Assign a function to be executed when the button is clicked
+button.addEventListener("click", determineConverter);
+
+
 function determineConverter () {
 //console.log(parseInt(document.getElementById("inputTemp").value));
 	var inputTemp = parseInt(document.getElementById("inputTemp").value);
@@ -32,15 +45,3 @@ function toFahrenheit (inputTemp) {
 function outPutToScreen (outTemp) {
 	document.getElementById("outputTemp").innerHTML = outTemp;
 }
-
-// Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
-
-// This function should determine which conversion should
-// happen based on which radio button is selected.
-//function determineConverter (clickEvent) {
-//  console.log("event", clickEvent);
-//}
-
-// Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter());
